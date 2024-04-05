aa61f8bdd6055879028e38977f8be659c677b44d
AlandvMarques <137972339+AlandvMarques@users.noreply.github.com>
2024-04-05 17:42:35 -0300

    avançando nas aulas de array

--- ProjetoAulas/Program.cs
+++ ProjetoAulas/Program.cs
@@ -191,8 +191,6 @@ string testen2 (){
     return primeiroN1 + " " + segundoN2;
 };
 
-*/
-
 
 
 //Criando Função que recebe parâmetros:
@@ -215,3 +213,147 @@ Console.WriteLine(nome);
 string nomeEIndade(string nome, int idade ){
     return "Meu nome é " + nome + " e tnho "+ idade;
 };
+
+*/
+
+// ArryList: 
+
+/*
+//array por indece
+using System.Collections;
+
+var arryList = new ArrayList();
+arryList.Add(1); //array 0;
+arryList.Add("Alan kevin"); //array 1;
+arryList.Add(true); //array 2
+
+Console.WriteLine(arryList[0]);
+Console.WriteLine(arryList[1]);
+Console.WriteLine(arryList[2]);
+
+Console.WriteLine("............");
+
+// ArrayList para Remover um Item
+arryList.RemoveAt(1);
+
+//ArrayList para Apagar Todos os Item:
+arryList.Clear();
+
+// ArrayList por Sequencia
+foreach(var intem in arryList)
+{
+    Console.WriteLine(intem);
+};
+
+Console.WriteLine("............");
+
+
+
+// Array tipado:
+var arryTipadoNumero = new int[4]{1,2,3,4};
+
+foreach (var item in arryTipadoNumero)
+{
+    Console.WriteLine(item);
+};
+
+Console.WriteLine("............");
+
+// -> alterar valor dentro do array
+var arryTipadoNumero2 = new int[4]{1,2,3,4};
+
+arryTipadoNumero2[2]= 10;
+
+
+foreach (var item2 in arryTipadoNumero2)
+{
+    Console.WriteLine(item2);
+};
+Console.WriteLine("............");
+
+
+// -> adicionando valores para o array
+var arryTipadoNumero3 = new int[4];
+
+arryTipadoNumero3[0]= 1;
+arryTipadoNumero3[1]= 5;
+arryTipadoNumero3[2]= 10;
+arryTipadoNumero3[3]= 15;
+
+foreach (var item3 in arryTipadoNumero3)
+{
+    Console.WriteLine(item3);
+};
+
+Console.WriteLine("............");
+
+// -> expandir o array com int
+var arryTipadoNumero4 = new int[4];
+
+arryTipadoNumero4[0]= 1;
+arryTipadoNumero4[1]= 5;
+arryTipadoNumero4[2]= 10;
+arryTipadoNumero4[3]= 15;
+
+Array.Resize(ref arryTipadoNumero4, 6 );
+
+arryTipadoNumero4[4]= 20;
+arryTipadoNumero4[5]= 25;
+
+foreach (var item4 in arryTipadoNumero4)
+{
+    Console.WriteLine(item4);
+};
+Console.WriteLine("............");
+
+
+// -> expandir o array com string
+var arryTipadoNumero5 = new string[4];
+
+arryTipadoNumero5[0]= "Alan";
+arryTipadoNumero5[1]= "kevin";
+arryTipadoNumero5[2]= "Marques";
+arryTipadoNumero5[3]= "De";
+
+Array.Resize(ref arryTipadoNumero5, 5 );
+
+arryTipadoNumero5[4]= "Oliveira";
+
+
+foreach (var item5 in arryTipadoNumero5)
+{
+    Console.WriteLine(item5);
+};
+Console.WriteLine("............");
+
+*/
+
+// Lista Genérica: 
+// Exemplo 1
+var lista = new List<string>(10)
+{
+    "Alan",
+    "Kevin",
+};
+var nome = lista[0];
+Console.WriteLine(nome);
+
+Console.WriteLine("............");
+
+// Exemplo 2
+
+var lista2 = new List<string>(10)
+{
+    "Alan",
+    "Kevin",
+};
+
+foreach (var item in lista)
+{
+    Console.WriteLine(item);
+};
+
+Console.WriteLine("............");
+
+
+// Exemple 
